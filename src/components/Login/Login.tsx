1edb94a2024e49f0fe44f861265173641be2d5aa
Umnichh <ololew22@gmail.com>
2025-02-23 15:52:04 +0300

    redux auth prototype

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import React, {useEffect, useState, useRef} from "react";
 import CalculatorSvg from "../../assets/Login/calculation-mathematics-calculator-svgrepo-com.svg?react";
 import CalendarSvg from "../../assets/Login/calendar-svgrepo-com.svg?react";
 import CheckSvg from "../../assets/Login/check-svgrepo-com.svg?react";
@@ -14,19 +14,57 @@ import Background2 from "../../assets/Login/background2.jpg";
 import Background3 from "../../assets/Login/background3.jpg";
 import Background4 from "../../assets/Login/background4.jpg";
 import Background5 from "../../assets/Login/background5.jpg";
+import store from "../../app/store";
 
 
 
 export default function Login() {
+
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  const [, setActiveImage] = useState(1);
   const [offset, setOffset] = useState({ x: 0, y: 0 });
-  const [backgroundList, setBackgroundList] = useState([
+  const [backgroundImages, setBackgroundList] = useState([
     {id: 0, src: Background1, isActive: false},
     {id: 1, src: Background2, isActive: false},
     {id: 2, src: Background3, isActive: false},
     {id: 3, src: Background4, isActive: false},
     {id: 4, src: Background5, isActive: false},
   ]);
-  const [activeImage, setActiveImage] = useState(1);
+
+  const backgroundSvgs = [
+    {id: 0, src: CalculatorSvg, className: 'calculator'},
+    {id: 1, src: CalendarSvg, className: 'calendar'},
+    {id: 2, src: CheckSvg, className: 'check'},
+    {id: 3, src: ClockSvg, className: 'clock'},
+    {id: 4, src: EmailSvg, className: 'email'},
+    {id: 5, src: FinanceSvg, className: 'finance'},
+    {id: 6, src: FlagSvg, className: 'flag'},
+    {id: 7, src: Graph1Svg, className: 'graph1'},
+    {id: 8, src: Graph2Svg, className: 'graph2'},
+  ]
+
+  function handleAuth(e: React.SyntheticEvent) {
+    e.preventDefault();
+    store.dispatch({type:'auth/login', payload:login});
+    store.dispatch({type:'auth/password', payload:password});
+    console.log(store.getState());
+  }
+
+  function handleBackground() {
+    setActiveImage(prev => {
+      const newActiveImage = (prev + 1) % backgroundImages.length;
+      setBackgroundList(prev =>
+        prev.map(item =>
+          ({...item, isActive: item.id === newActiveImage})));
+      return newActiveImage;
+    });
+  }
+
+  useEffect(() => {
+    const imageInterval = setInterval(handleBackground, 5000);
+    return () => clearInterval(imageInterval);
+  }, []);
 
   useEffect(() => {
     function handleMouseMove(event: MouseEvent) {
@@ -36,87 +74,41 @@ export default function Login() {
       setOffset({ x, y });
     }
 
-    function handleBackground() {
-      setActiveImage(prev => {
-        const newActiveImage = (prev + 1) % backgroundList.length;
-        setBackgroundList(prev =>
-          prev.map(item =>
-            ({...item, isActive: item.id === newActiveImage})));
-        return newActiveImage;
-      });
-    }
-    const imageInterval = setInterval(handleBackground, 5000);
-    handleBackground();
     window.addEventListener("mousemove", handleMouseMove);
-    return () => {
-      window.removeEventListener("mousemove", handleMouseMove)
-      clearInterval(imageInterval)
-    };
+    handleBackground();
+    return () => window.removeEventListener("mousemove", handleMouseMove)
   }, []);
-
-  useEffect(() => {
-    console.log(activeImage);
-  }, [activeImage]);
 
   return (
     <div className="login">
       <div className="login__background-container">
-        {backgroundList.map((item) =>
+        {backgroundImages.map((item) =>
           <img src={item.src}
                alt="Фон"
                className={`login__background ${item.isActive ? 'login__background--active' : ''}`}
                key={item.id}
                style={{ transform: `translate(${offset.x * 10}px, ${offset.y * 10}px)`}}
           />)}
-        <CalculatorSvg
-          className="login__background--calculator"
-          style={{ transform: `translate(${offset.x * 150}px, ${offset.y * 150}px)` }}
-        />
-        <CalendarSvg
-          className="login__background--calendar"
-          style={{ transform: `translate(${offset.x * 100}px, ${offset.y * 100}px)` }}
-        />
-        <CheckSvg
-          className="login__background--check"
-          style={{ transform: `translate(${offset.x * 50}px, ${offset.y * 50}px)` }}
-        />
-        <ClockSvg
-          className="login__background--clock"
-          style={{ transform: `translate(${offset.x * 25}px, ${offset.y * 25}px)` }}
-        />
-        <EmailSvg
-          className="login__background--email"
-          style={{ transform: `translate(${offset.x * 75}px, ${offset.y * 75}px)` }}
-        />
-        <FinanceSvg
-          className="login__background--finance"
-          style={{ transform: `translate(${offset.x * 125}px, ${offset.y * 125}px)` }}
-        />
-        <FlagSvg
-          className="login__background--flag"
-          style={{ transform: `translate(${offset.x * 175}px, ${offset.y * 175}px)` }}
-        />
-        <Graph1Svg
-          className="login__background--graph1"
-          style={{ transform: `translate(${offset.x * 200}px, ${offset.y * 200}px)` }}
-        />
-        <Graph2Svg
-          className="login__background--graph2"
-          style={{ transform: `translate(${offset.x * 225}px, ${offset.y * 225}px)` }}
-        />
+        {backgroundSvgs.map(({ id, src: SvgComponent, className }, index) => (
+          <SvgComponent
+            key={id}
+            className={`login__background--${className}`}
+            style={{ transform: `translate(${offset.x * (index + 2) * 25}px, ${offset.y * (index + 2) * 25}px)` }}
+          />
+        ))}
       </div>
       <div  className="login__container">
         <Logo isWhiteTheme={true} logoWidth="100px" logoHeight="100px" nameWidth="100%" nameHeight="150px"/>
         <form action="" className="login__form" autoComplete="off">
           <fieldset>
             <legend className="login__legend">Логин</legend>
-            <input name="login" id="login" type="text" placeholder="ivanov@gmail.com" className="login__input" />
+            <input onChange={(e) => setLogin(e.target.value)} name="login" id="login" type="text" placeholder="ivanov@gmail.com" className="login__input" />
           </fieldset>
           <fieldset>
             <legend className="login__legend">Пароль</legend>
-            <input name="password" id="password" type="password" placeholder="strongpassword123" className="login__input" />
+            <input onChange={(e) => setPassword(e.target.value)}  name="password" id="password" type="password" placeholder="strongpassword123" className="login__input" />
           </fieldset>
-          <button type="submit" className="login__button">Войти</button>
+          <button type="submit" onClick={(e) => handleAuth(e)} className="login__button">Войти</button>
         </form>
       </div>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import store from './app/store';
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
         <App/>
-    </BrowserRouter>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 )
