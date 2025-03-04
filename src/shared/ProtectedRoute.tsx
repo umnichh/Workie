a7b08115d98e66920ebae293b5a135c1488f6f76
Umnichh <ololew22@gmail.com>
2025-03-04 21:38:35 +0300

    token cookie

--- src/shared/ProtectedRoute.tsx
+++ src/shared/ProtectedRoute.tsx
@@ -2,7 +2,7 @@ import {Outlet} from "react-router-dom";
 import Cookies from "js-cookie";
 
 export default function ProtectedRoute() {
-  const token = Cookies.get("token");
+  const token = Cookies.get("set-token");
   console.log(token);
   // if (!isAuthenticated) {
   //   return <Navigate to="/login" />;
