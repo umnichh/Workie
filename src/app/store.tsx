7bdf80423c4a288714be9ee6ee8480296598bb72
Umnichh <ololew22@gmail.com>
2025-03-06 21:54:12 +0300

    remove authcontext

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -1,23 +1,23 @@
-// import {configureStore, PayloadAction} from "@reduxjs/toolkit";
+import {configureStore, PayloadAction} from "@reduxjs/toolkit";
 
-// type initialType = {
-//   userLogin: string,
-//   userPassword: string,
-// }
-// const initialState : initialType = {
-//   userLogin: "",
-//   userPassword: "",
-// }
-// function userReducer(state = initialState, action: PayloadAction<string>) {
-//   switch (action.type) {
-//     case 'auth/login':
-//       return {...state, userLogin: action.payload};
-//     case 'auth/password':
-//       return {...state, userPassword: action.payload};
-//     default:
-//       return state;
-//   }
-// }
+type initialType = {
+  userLogin: string,
+  userPassword: string,
+}
+const initialState : initialType = {
+  userLogin: "",
+  userPassword: "",
+}
+function userReducer(state = initialState, action: PayloadAction<string>) {
+  switch (action.type) {
+    case 'auth/login':
+      return {...state, userLogin: action.payload};
+    case 'auth/password':
+      return {...state, userPassword: action.payload};
+    default:
+      return state;
+  }
+}
 
-// const store = configureStore({ reducer: userReducer});
-// export default store;
+const store = configureStore({ reducer: userReducer});
+export default store;
