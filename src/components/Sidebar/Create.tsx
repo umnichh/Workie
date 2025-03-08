ff3236feb971aa358bce869f597576d7b18d4e68
Umnichh <ololew22@gmail.com>
2025-03-08 20:04:41 +0300

    Fix wrong project format

--- src/components/Sidebar/Create.tsx
+++ src/components/Sidebar/Create.tsx
@@ -2,33 +2,45 @@ import { useState } from "react";
 import { useUIContext } from "../../hooks/useUIContext";
 import { useFetch } from "../../hooks/useFetch";
 
-export default function Create() {
-  const [projectName, setProjectName] = useState('');
-  const { isCreate } = useUIContext();
-  const { fetchData } = useFetch({
-    api: '/project',
-    method: 'POST',
-    isBody: true
-});
-  const {setIsCreate, setIsCreateDialog} = useUIContext();
+interface ProjectData {
+  name: string;
+}
 
+export default function Create() {
+  const [projectName, setProjectName] = useState("");
+  const { isCreate } = useUIContext();
+  const { fetchData } = useFetch<ProjectData>({
+    api: "/project",
+    method: "POST",
+    isBody: true,
+  });
+  const { setIsCreate, setIsCreateDialog } = useUIContext();
 
   const handleClick = () => {
-    if (projectName === ''){
-      return null
+    if (projectName === "") {
+      return null;
     }
-    fetchData.mutate(projectName);
-    setIsCreate('')
-    setIsCreateDialog(false)
-  }
+
+    const projectData: ProjectData = {
+      name: projectName,
+    };
+
+    fetchData.mutate(projectData);
+    setIsCreate("");
+    setIsCreateDialog(false);
+  };
 
   return (
     <div
       className={`project__options ${isCreate ? "project__options--show" : ""}`}
     >
-      <label htmlFor="project__label">Название проека</label>
-      <input onChange={(e) => setProjectName(e.target.value)} type="text" name="project__input" />
-      <button className="project__create" onClick={handleClick}>Создать</button>
+      <label htmlFor="project__label">Название проекта</label>
+      <input
+        onChange={(e) => setProjectName(e.target.value)}
+        type="text"
+        name="project__input"
+      />
+      <button onClick={handleClick}>Создать</button>
     </div>
   );
 }
